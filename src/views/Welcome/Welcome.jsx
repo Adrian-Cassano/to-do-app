c602a72a3d3ae0d71a86d4c3f2da4b90b2e96df0
Adrian-Cassano <adriandcassano@hotmail.com>
2023-01-27 22:30:36 -0300

    feat: add validations and navigate

--- src/views/Welcome/Welcome.jsx
+++ src/views/Welcome/Welcome.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate  } from "react-router-dom";
 
 import { setName } from "../../redux/slices/userSlice";
 
@@ -8,16 +9,26 @@ import styles from "./Welcome.module.css";
 const Welcome = () => {
   const dispatch = useDispatch();
   const name = useSelector((store) => store.userSlice);
-
+  const navigate = useNavigate();
+  
   const handleSubmit = (e) => {
     e.preventDefault();
+    const largo =  e.target[0].value.length
     const name = e.target[0].value;
-    if (name === "") {
+    if (name === "" ) {
       alert("Completa tu nombre");
-    } else {
-      dispatch(setName(name));
+    }else if (largo < 4 ){
+      alert("Minimo 4 letras")
+    }
+    else if (largo >= 18){
+      alert ("Maximo 18")
+    }
+     else {
+      dispatch(setName(name)); 
+      navigate("/Home");
     }
   };
+  
   return (
     <div id={styles.MasterContainer}>
       <h1>TO-DO-APP</h1>
@@ -27,12 +38,10 @@ const Welcome = () => {
           tareas diarias!
         </div>
         <div id={styles.TextoName}>Ingresa tu nombre</div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="nombre" placeholder="ingresar" />
-          <input type="submit" name="submit" />
+        <form id={styles.FormContainer}   onSubmit={handleSubmit}>
+          <input  id={styles.InputName} type="text" name="nombre" placeholder="Ingresa tu nombre" />
+          <button id={styles.ButtonIn}   type="submit" name="submit" >Ingresar</button>
         </form>
-
-        <button id={styles.ButtonIn}>Ingresar</button>
       </div>
     </div>
   );
