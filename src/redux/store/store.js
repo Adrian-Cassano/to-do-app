9d0d75c1ca095cb6e23c7cb9ead364c60d238710
Adrian-Cassano <adriandcassano@hotmail.com>
2023-02-06 20:18:40 -0300

    feat: add modal update

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../slices/userSlice';
+import calendarSlice from '../slices/calendarSlice';
 
 const store = configureStore({
   reducer: {
     userSlice,
+    calendarSlice
     
   },
 });
