b091dbbf0f8e1950d1b352a550d2a6b5c16c646b
Adrian-Cassano <adriandcassano@hotmail.com>
2023-02-27 21:44:44 -0300

    feature: add set clear setUp and set down in slice

--- src/redux/slices/modalSlice.js
+++ src/redux/slices/modalSlice.js
@@ -1,12 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 //COMO INICIA Y ES EL ESTADO, QUE SE VA CAMBIANDO CON EL setFavorito
 const initialState = {
-  name: null,
-  description: null,
-  calenderStart: null,
-  calenderEnd: null,
-  tag: null,
+  tareas: [],
+  ultimoId: 0,
 };
 
 // EL SLICE EN SI, QUE IMPORTA EL INITIAL STATE
@@ -16,28 +14,64 @@ export const modalSlice = createSlice({
 
   //Los reductores lo que hacen es modificar los valores del estado
   reducers: {
-    setName: (state, action) => {
-      state.name =  [action.payload];
-    },
-    setDescription: (state, action) => {
-      state.description =  [action.payload];
-    },
-    setCalenderStart: (state, action) => {
-      state.calenderStart =  [action.payload];
-    },
-    setCalenderEnd: (state, action) => {
-      state.calenderEnd =  [action.payload];
-    },
-    setTag: (state, action) => {
-      state.tag =  [action.payload];
-    },
+    setTareas: (state, action) => {
+      const { title, description, dateStart, dateEnd } = action.payload;
 
+      state.tareas = [
+        ...state.tareas,
+        {
+          title: title,
+          description: description,
+          startDate: dateStart,
+          endDate: dateEnd,
+          id: state.ultimoId,
+        },
+      ];
+      state.ultimoId++;
+    },
+    setClear: (state, action) => {
+      const newArray = state.tareas.filter((tarea) => {
+        return action.payload !== tarea.id;
+      });
+      state.tareas = newArray;
+    },
+    setMoveUp: (state, action) => {
+      // const actual = action.payload
+      // const idActual = action.payload.id
+      const newArray = state.tareas.filter((tarea, index) => {
+        
+
+        return action.payload.id !== tarea.id 
+        
+      });
+      state.tareas = newArray;
+      
+      state.tareas.splice(action.payload.id-1,0, action.payload)
+      console.log(action.payload.id)
+      
+      
+    },
+    setMoveDown: (state, action) => {
+      // const actual = action.payload
+      // const idActual = action.payload.id
+      console.log(action.payload)
+      const newArray = state.tareas.forEach((tarea, index) => {
+        
+        
+        
+        return  state.tareas.splice(action.payload.id, 2  ,action.payload)
+        
+      });
+      // state.tareas = newArray;
+      
+      // // console.log(action.payload.id)
+     
+    },
     
   },
 });
 
-export const { setName, setDescription, setCalenderStart, setCalenderEnd, setTag} = modalSlice.actions;
-
+export const { setTareas, setClear, setMoveDown,setMoveUp } = modalSlice.actions;
 
 export const stateday = (state) => state;
 
